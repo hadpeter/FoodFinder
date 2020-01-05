71217622c285925ce69c29925bb22d6aaf0402de
hadpeter <hadpeter@umich.edu>
2020-01-05 16:31:27 -0500

    stars update upon click

--- FoodTracker/FoodTracker/RatingControl.swift
+++ FoodTracker/FoodTracker/RatingControl.swift
@@ -11,7 +11,12 @@ import UIKit
     
     //MARK: Properties
     private var ratingButtons = [UIButton]()
-    var rating = 0
+    // rating has a property observer
+    var rating = 0 {
+        didSet {
+            updateButtonSelectionStates()
+        }
+    }
     @IBInspectable var starSize: CGSize = CGSize(width: 44.0, height: 44.0) {
         didSet {
             setupButtons()
@@ -37,7 +42,20 @@ import UIKit
     
     //MARK: Button Action
     @objc func ratingButtonTapped(button: UIButton) {
-        print("Button pressed 👍")
+        guard let index = ratingButtons.firstIndex(of: button) else {
+            fatalError("The button, \(button), is not in the ratingButtons array: \(ratingButtons)")
+        }
+        
+        // Calculate the rating of the selected button
+        let selectedRating = index + 1
+        
+        if selectedRating == rating {
+            // If the selected star represents the current rating, reset the rating to 0.
+            rating = 0
+        } else {
+            // Otherwise set the rating to the selected star
+            rating = selectedRating
+        }
     }
     
     //MARK: Private Methods
@@ -78,6 +96,15 @@ import UIKit
             
             // Add the new button to the rating button array
             ratingButtons.append(button)
+        }
+        updateButtonSelectionStates()
+    }
+    
+    // update selection state of buttons
+    private func updateButtonSelectionStates() {
+        for (index, button) in ratingButtons.enumerated() {
+            // If the index of a button is less than the rating, that button should be selected.
+            button.isSelected = index < rating
         }
     }
 }
