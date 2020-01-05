b9016d859500df1be344a5a900ca11ae4e7d0837
hadpeter <hadpeter@umich.edu>
2020-01-05 13:21:23 -0500

    created RatingControl subclass

---
+++ FoodTracker/FoodTracker/RatingControl.swift
@@ -0,0 +1,19 @@
+//
+//  RatingControl.swift
+//  FoodTracker
+//
+//  Created by Hadley Peterson on 1/5/20.
+//
+
+import UIKit
+
+class RatingControl: UIStackView {
+    //MARK: Initialization
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+}
