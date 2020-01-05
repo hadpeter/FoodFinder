996b23460cb2d475f6d63dc083a110ebb7f6688a
hadpeter <hadpeter@umich.edu>
2020-01-05 14:56:07 -0500

    first button created

--- FoodTracker/FoodTracker/RatingControl.swift
+++ FoodTracker/FoodTracker/RatingControl.swift
@@ -11,9 +11,24 @@ class RatingControl: UIStackView {
     //MARK: Initialization
     override init(frame: CGRect) {
         super.init(frame: frame)
+        setupButtons()
     }
     
     required init(coder: NSCoder) {
         super.init(coder: coder)
+        setupButtons()
+    }
+    
+    //MARK: Private Methods
+    private func setupButtons() {
+        // Create red button
+        let button = UIButton()
+        button.backgroundColor = UIColor.red
+        // Add button constraints
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.heightAnchor.constraint(equalToConstant: 44.0).isActive = true
+        button.widthAnchor.constraint(equalToConstant: 44.0).isActive = true
+        // Add the button to the stack
+        addArrangedSubview(button)
     }
 }
