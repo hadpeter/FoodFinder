dd4611c2b51d01fe1f01c578601dd2424d8f7d00
hadpeter <hadpeter@umich.edu>
2020-01-11 14:58:17 -0500

    image label and rating added to content view

---
+++ FoodTracker/FoodTracker/MealTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MealTableViewCell.swift
+//  FoodTracker
+//
+//  Created by Hadley Peterson on 1/11/20.
+//  Copyright © 2020 SideProj. All rights reserved.
+//
+
+import UIKit
+
+class MealTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
